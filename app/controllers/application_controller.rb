53acc42bbcddea7708255602c0f5498f930f6ca1
Charlie <peabody00@gmail.com>
2021-02-17 10:33:51 -0600

    commit tree b1c804e9ad3b72ad4a221ac3e95ab9aacd5d1df0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+      end
+  
+      post '/teams' do
+        @team = Team.new(params[:team])
+  
+        params[:team][:heros].each do |member|
+          Member.new(member)
+        end
+  
+        @members = Member.all
+  
+        erb :team
+      end
+
 
 end
